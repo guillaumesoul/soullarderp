117bbc94966d03d080231e100744a13fd11400a1
guillaumesoullard <guillaume.soullard@gmail.com>
2019-12-15 23:30:40 +0100

    serialization init

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
      */
-    public function index()
+    public function index(SerializerInterface $serializer)
     {
-        return $this->render('base.html.twig');
+        $user = $this->getDoctrine()->getRepository(User::class)->find(1);
+        $json = $serializer->serialize(
+            $user,
+            'json', ['groups' => 'user']
+        );
+        //return $this->render('base.html.twig');
+        return new JsonResponse($json);
     }
 }
